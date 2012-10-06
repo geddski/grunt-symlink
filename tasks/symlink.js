b75b1e1e7ed9b75cb17411f4061780854e76faf7
Dave Geddes <davidcgeddes@gmail.com>
2012-10-06 00:27:23 -0600

    better logging output

--- tasks/symlink.js
+++ tasks/symlink.js
@@ -14,6 +14,11 @@ module.exports = function(grunt) {
     links.forEach(function(link){
       try{
         fs.symlinkSync(link.to, link.link, link.type || 'file');
+        var rel = link.link.substr(0, link.link.lastIndexOf('/') + 1);
+        grunt.log.ok('created symlink at ' + link.link + 
+          ' that points to ' + link.to +
+          ' (relative to ' + rel +')'
+        );
       }
       catch(e){
         if (e.code === 'EEXIST') grunt.log.error(link.link + ' already exists, skipping');
